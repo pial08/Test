5dd4b876c7163d766f158efa8119c57767841175
pial08 <pialcsedu19@gmail.com>
2017-11-20 02:21:45 +0600

    lalala

--- blog_post/models.py
+++ blog_post/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 class Post(models.Model):
 
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=2200)
     description = models.TextField()
 
     def __str__(self):
