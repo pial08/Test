65a3cb65a203058d090f54fa772f1f4b4c68e4a1
pial08 <pialcsedu19@gmail.com>
2017-11-24 21:36:55 +0600

    extra print removed

--- blog_post/views.py
+++ blog_post/views.py
@@ -4,6 +4,4 @@ from .models import Post
 
 def home(request):
     all_post = Post.objects.all()
-    print(all_post)
-    print("lalala")
     return render(request, 'index.html', {'all_post_list': all_post})
