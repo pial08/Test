d878103c5e61b51f32f027c8734ce6f6586cc0e6
pial08 <pialcsedu19@gmail.com>
2017-11-24 22:44:15 +0600

    new view added

--- blog_post/views.py
+++ blog_post/views.py
@@ -5,3 +5,8 @@ from .models import Post
 def home(request):
     all_post = Post.objects.all()
     return render(request, 'index.html', {'all_post_list': all_post})
+
+
+def post_list(request):
+    post_list = Post.objects.all()
+    return render(request, post_list.html, {'post_list' : post_list()})
