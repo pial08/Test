fcc50b551f88d6fd4f90c5387a89750d07286e7b
pial08 <pialcsedu19@gmail.com>
2018-04-02 16:50:11 +0600

    test commit

--- Test/settings.py
+++ Test/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'Test.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.sqlite31',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
