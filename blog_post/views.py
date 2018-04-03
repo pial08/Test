8cdb371b775301e38d48820a1da72cafb0a8cafd
pial08 <pialcsedu19@gmail.com>
2018-04-03 21:54:30 +0600

    commit

--- blog_post/views.py
+++ blog_post/views.py
@@ -11,3 +11,8 @@ def post_list(request):
     post_list = Post.objects.all()
     return render(request, 'post_list.html', {'post_list':post_list})
 
+
+def single_post(request):
+    post = Post.objects.get(pk=3)
+    print(post)
+    return render(request, 'single_post.html', {'post': post})
