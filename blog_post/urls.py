a0a2406ad2f2a8875b801b248352515f699d5d5c
pial08 <pialcsedu19@gmail.com>
2018-04-06 20:49:41 +0600

    commit

--- blog_post/urls.py
+++ blog_post/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url, include
 from blog_post import views
 
 urlpatterns = [
-    url(r'^/post_list', views.post_list, name='post_list')
+    url(r'^post_list', views.post_list, name='post_list')
 ]
