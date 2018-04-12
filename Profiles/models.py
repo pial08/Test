a3ce097a8f08f3a7e06957e18a2ed055312c2889
pial08 <pialcsedu19@gmail.com>
2018-04-13 00:33:25 +0600

    Model for users created

--- Profiles/models.py
+++ Profiles/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    #user_id = models.AutoField(primary_key=true)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    user_address = models.CharField(max_length=200)
+    user_birthdate = models.DateField()
+    user_password = models.CharField(max_length=50)
